78cd09530a688a5fa03db7e09708276416df160d
Mutwingima Mariam <princessmirina45@gmail.com>
2025-09-09 11:53:05 +0200

    Updated 2-arguments.js file

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,12 +1,13 @@
-let number;
 function findArg(x) {
-  if (x != number) {
+  if (x == 0) {
     console.log("No argument");
-  } else if (x == number) {
+  } else if (x == 1) {
     console.log("Argument found");
   } else {
     console.log("Arguments found");
   }
 }
 
-findArg();
+findArg(0);
+findArg(1);
+findArg(5);
